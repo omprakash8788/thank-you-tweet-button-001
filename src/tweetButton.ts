7b9d44e506bcd18eaf2e875bb2446c2999d5d0a6
omprakash8788 <omprakashkumar552@gmail.com>
2025-10-25 22:11:44 +0530

    refactor: change div to section element

--- src/tweetButton.ts
+++ src/tweetButton.ts
@@ -1,12 +1,13 @@
 
 export function tweetButton(){
-    const tweetButtonContainer=document.createElement("div");
+    const tweetButtonContainer = document.createElement("section");
     const anchorElement=document.createElement("a");
     const anchorElementContent=document.createTextNode("Send a thank you tweet");
 
     
     const tweetUrl =
      "https://twitter.com/intent/tweet?text=https%3A%2F%2Fgithub.com%2Fomprakash8788%2Fbutton-001"
+
     tweetButtonContainer.setAttribute("id", "tweet-btn-container");
     anchorElement.setAttribute("class", "tweet-button");
     anchorElement.setAttribute("target", "_blank");
